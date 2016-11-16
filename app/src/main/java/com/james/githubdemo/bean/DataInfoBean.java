db20f8702850b869c6846cae862e55af1e8d1942
whatmeever <zzp__2001@sina.com>
2016-11-16 23:23:28 +0800

    add mvp

---
+++ app/src/main/java/com/james/githubdemo/bean/DataInfoBean.java
@@ -0,0 +1,7 @@
+package com.james.githubdemo.bean;
+
+/**
+ * Created by james on 2016/11/16.
+ */
+public class DataInfoBean {
+}
---
+++ app/src/main/java/com/james/githubdemo/model/IService.java
@@ -0,0 +1,7 @@
+package com.james.githubdemo.model;
+
+/**
+ * Created by james on 2016/11/16.
+ */
+public interface IService {
+}
